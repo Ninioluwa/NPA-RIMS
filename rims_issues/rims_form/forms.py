5a3bacd877868cb288427fef414311f30a4e34a5
Ninioluwa <oluwatunmininu29603@gmail.com>
2022-06-20 16:53:26 +0100

    Created rims Form class

---
+++ rims_issues/rims_form/forms.py
@@ -0,0 +1,24 @@
+
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from .models import IssuesModel
+
+User = get_user_model()
+
+class RimsForm(forms.Form):
+    subject = forms.CharField(widget=forms.TextInput (attrs={'class': 'border-2'}))
+    status = forms.CharField(widget=forms.TextInput (attrs={'class': 'border-2'}))
+    sen_no = forms.CharField(widget=forms.TextInput (attrs={'class': 'border-2'}))
+    issues = forms.CharField(widget=forms.Textarea (attrs={'class': 'border-2'}))
+
+class RimsModelForm(forms.ModelForm):
+    class Meta:
+        model = IssuesModel
+        fields = {
+            'subject',
+            'sen_no',
+            'status',
+            'issues',
+        }
+ 

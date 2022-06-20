0bb0013001f90c7ead73891f2d5db6af71b271e0
Ninioluwa <oluwatunmininu29603@gmail.com>
2022-06-20 16:21:16 +0100

    Registered models in admin

--- rims_issues/rims_form/admin.py
+++ rims_issues/rims_form/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import IssuesModel
+from .models import Profile
+from .models import Branch
+from .models import User
+
+admin.site.register(IssuesModel)
+admin.site.register(Profile)
+admin.site.register(Branch)
+admin.site.register(User)

3a0b50c7bc077cd502d49dcca567325a88fadd70
toluhunter <toluhunter19@gmail.com>
2022-06-22 14:01:55 +0100

    added login url field and removed extra newline

--- rims_issues/rims_issues/settings.py
+++ rims_issues/rims_issues/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'rims_issues.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / "templates" ],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/dashboard"
 AUTH_USER_MODEL = 'rims_form.User'
+LOGIN_URL = '/login'

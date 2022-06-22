946873c508f6f955c9d49d6207366d645a1c37e9
toluhunter <toluhunter19@gmail.com>
2022-06-22 13:56:50 +0100

    Removed Profile and Branch models, added custom user manager

--- rims_issues/rims_form/models.py
+++ rims_issues/rims_form/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.utils.translation import gettext_lazy as _
 
 
 # many issues to a port
+class CustomManager(UserManager):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        super().create_superuser(username, email, password, **extra_fields)
+
 
 class User(AbstractUser):
-    pass
-
-    def __str__(self):
-        return self.email
-
-class Branch(models.Model):
-     # Port Choices
     CHOICES = [
         ('LAGOS', 'Lagos'),
         ('TIN-CAN', 'Tin-Can'),
@@ -20,32 +19,30 @@ class Branch(models.Model):
         ('CALABAR', 'Calabar'),
         ('ONNE', 'Onne')
     ]
-    port =  models.CharField(max_length = 10, choices = CHOICES)
+    port = models.CharField(max_length=10, choices=CHOICES)
+    is_active = models.BooleanField(
+        _("active"),
+        default=False,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    REQUIRED_FIELDS = ["email", "port"]
+    objects = CustomManager()
 
     def __str__(self):
-        return self.get_port_display()
-
-
+        return self.email
 
 
 class IssuesModel(models.Model):
 
-    port = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    sen_no = models.CharField(max_length = 20)
-    status = models.CharField(max_length = 20)
-    subject = models.CharField(max_length = 100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sen_no = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
+    subject = models.CharField(max_length=100)
     issues = models.TextField()
-    date_submitted = models.DateField(auto_now_add= True)
+    date_submitted = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.subject
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    access = models.BooleanField(default = False)
-    port = models.ForeignKey(Branch, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.email
-

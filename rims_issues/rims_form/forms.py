ff7e7d4ee6df3c61f1a5a4c02ec7e482ecd830a9
toluhunter <toluhunter19@gmail.com>
2022-06-22 14:02:56 +0100

    refactored and added port to fields

--- rims_issues/rims_form/forms.py
+++ rims_issues/rims_form/forms.py
@@ -1,4 +1,3 @@
-
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
@@ -6,11 +5,17 @@ from .models import IssuesModel
 
 User = get_user_model()
 
+
 class RimsForm(forms.Form):
-    subject = forms.CharField(widget=forms.TextInput (attrs={'class': 'border-2'}))
-    status = forms.CharField(widget=forms.TextInput (attrs={'class': 'border-2'}))
-    sen_no = forms.CharField(widget=forms.TextInput (attrs={'class': 'border-2'}))
-    issues = forms.CharField(widget=forms.Textarea (attrs={'class': 'border-2'}))
+    subject = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'border-2'}))
+    status = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'border-2'}))
+    sen_no = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'border-2'}))
+    issues = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'border-2'}))
+
 
 class RimsModelForm(forms.ModelForm):
     class Meta:
@@ -21,12 +26,10 @@ class RimsModelForm(forms.ModelForm):
             'status',
             'issues',
         }
- 
 
- 
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-
-        fields = ("username", "email" )
+        fields = ("username", "email", "port")
         field_classes = {'username': UsernameField}
